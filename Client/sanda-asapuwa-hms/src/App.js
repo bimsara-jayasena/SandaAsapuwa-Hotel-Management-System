b221d231aceb49998777f3defce8fd4dcdf70ed2
Bimsara-D <bimsarajayasena06@gmail.com>
2024-04-17 02:42:24 +0530

    first commit

---
+++ Client/sanda-asapuwa-hms/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Home from './Pages/Home';
+import CreateAccount from './Pages/AccoutnCreaction';
+import {Routes,Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+     <Routes>
+        
+       <Route path='/' element={<Home/>}/>
+       <Route path='/New Account' element={<CreateAccount/>}/>
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
