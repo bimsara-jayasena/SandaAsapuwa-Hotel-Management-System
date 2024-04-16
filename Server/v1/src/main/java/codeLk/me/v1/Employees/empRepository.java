a05973523fbdb23118ec92dd3c1e674a8a8609c2
Bimsara Jayasena <129860594+DragonBcode@users.noreply.github.com>
2024-04-17 02:30:38 +0530

    Delete Server directory

--- Server/v1/src/main/java/codeLk/me/v1/Employees/empRepository.java
+++
@@ -1,9 +0,0 @@
-package codeLk.me.v1.Employees;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface empRepository extends MongoRepository<Employees, ObjectId> {
-}
